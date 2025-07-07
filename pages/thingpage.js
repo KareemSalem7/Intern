7af28175cd62d8b50004634eef89897ee4f6c7a7
KareemSalem7 <kareem.salem2003@gmail.com>
2025-07-07 01:36:42 -0400

    added signout

--- pages/thingpage.js
+++ pages/thingpage.js
@@ -1,5 +1,17 @@
+import React from 'react';
 import ThingsList from '../components/ThingsList';
+import Header from '../components/Header';
+import { UserProvider } from '../lib/UserContext';
 
 export default function Home() {
-  return <ThingsList />;
+  return (
+    <UserProvider>
+      <div className="flex min-h-screen flex-col">
+        <Header />
+        <main className="flex-1">
+          <ThingsList />
+        </main>
+      </div>
+    </UserProvider>
+  );
 }
